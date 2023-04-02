a3c20d225eee2b56bb575abc9128e0cbf79dafaf
kumawatlalit007 <kumawatlalit912@gmail.com>
2023-04-02 22:42:43 +0530

    added-redux-code

---
+++ src/redux/reducers/reducer.js
@@ -0,0 +1,60 @@
+const INIT_STATE = {
+  carts: [],
+};
+
+export const cartreducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case "ADD_CART":
+      const ItemIndex = state.carts.findIndex(
+        (Item) => Item.id === action.payload.id
+      );
+
+      if (ItemIndex >= 0) {
+        state.carts[ItemIndex].qnty += 1;
+        return {
+          ...state,
+          carts: [...state.carts],
+        };
+      } else {
+        const temp = { ...action.payload, qnty: 1 };
+        return {
+          ...state,
+          carts: [...state.carts, temp],
+        };
+      }
+
+    case "RMV_CART":
+      const data = state.carts.filter((el) => el.id !== action.payload);
+      // console.log(data);
+
+      return {
+        ...state,
+        carts: data,
+      };
+
+    case "RMV_ONE":
+      const ItemIndex_dec = state.carts.findIndex(
+        (Item) => Item.id === action.payload.id
+      );
+
+      if (state.carts[ItemIndex_dec].qnty >= 1) {
+        const dltItems = (state.carts[ItemIndex_dec].qnty -= 1);
+        console.log([...state.carts, dltItems]);
+
+        return {
+          ...state,
+          carts: [...state.carts],
+        };
+      } else if (state.carts[ItemIndex_dec].qnty === 1) {
+        const data = state.carts.filter((el) => el.id !== action.payload);
+
+        return {
+          ...state,
+          carts: data,
+        };
+      }
+
+    default:
+      return state;
+  }
+};
